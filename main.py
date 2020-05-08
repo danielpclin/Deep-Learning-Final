dd4ab55ab0a83fde6b0475ebc43cfa82153bee46
danielpclin <daniel.pclin@gmail.com>
2020-05-09 05:09:31 +0800

    update files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tensorflow as tf
+from keras.utils import to_categorical
 from keras_preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Input, Model
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
@@ -23,31 +24,29 @@ if gpus:
 
 def train():
     epochs = 50
-    batch_size = 50
+    batch_size = 100
     img_width = 200
     img_height = 60
-    train_dir = 'train/data01_train'
-    test_dir = 'train/data01_train'
     alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+    int_to_char = dict((i, c) for i, c in enumerate(alphabet))
     df = pd.read_csv('train/data01_train.csv', delimiter=',')
     df['code'] = df['code'].apply(lambda el: list(el))
-    print(df)
+    df[[f'code{i}' for i in range(1, 7)]] = pd.DataFrame(df['code'].to_list(), index=df.index)
+    for i in range(1, 7):
+        df[f'code{i}'] = df[f'code{i}'].apply(lambda el: to_categorical(char_to_int[el], len(alphabet)))
     datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.2)
     train_generator = datagen.flow_from_dataframe(dataframe=df, directory="train/data01_train", subset='training',
-                                                  x_col="filename", y_col="code",
-                                                  class_mode="categorical", classes=alphabet,
+                                                  x_col="filename", y_col=[f'code{i}' for i in range(1, 7)],
+                                                  class_mode="multi_output",# classes=alphabet,
                                                   target_size=(img_height, img_width), batch_size=batch_size)
     valid_generator = datagen.flow_from_dataframe(dataframe=df, directory="train/data01_train", subset='validation',
-                                                  x_col="filename", y_col="code",
-                                                  class_mode="categorical", classes=alphabet,
+                                                  x_col="filename", y_col=[f'code{i}' for i in range(1, 7)],
+                                                  class_mode="multi_output",# classes=alphabet,
                                                   target_size=(img_height, img_width), batch_size=batch_size)
-    # print(next(train_generator))
-    # exit()
-    # train_num = len(os.listdir(train_dir))
-    # test_num = len(os.listdir(test_dir))
     input_shape = (img_height, img_width, 3)
     checkpoint_path = 'checkpoint'
-    log_dir = 'logs/run21'
+    log_dir = 'logs/1'
     main_input = Input(shape=input_shape)
     x = Conv2D(filters=32,
                kernel_size=(3, 3),
@@ -55,10 +54,10 @@ def train():
                activation='relu')(main_input)
     x = Conv2D(filters=32,
                kernel_size=(3, 3),
-               activation='relu')(x)
+               activation='relu')(main_input)
     # x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    # x = Dropout(0.2)(x)
+    x = Dropout(0.2)(x)
     x = Conv2D(filters=64,
                kernel_size=(3, 3),
                padding='same',
@@ -69,31 +68,33 @@ def train():
     # x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
     x = Dropout(0.2)(x)
-    # x = Conv2D(filters=128,
-    #            kernel_size=(3, 3),
-    #            padding='same',
-    #            activation='relu')(x)
-    # x = Conv2D(filters=128,
-    #            kernel_size=(3, 3),
-    #            activation='relu')(x)
+    x = Conv2D(filters=128,
+               kernel_size=(3, 3),
+               padding='same',
+               activation='relu')(x)
+    x = Conv2D(filters=128,
+               kernel_size=(3, 3),
+               activation='relu')(x)
     # x = BatchNormalization()(x)
-    # x = MaxPooling2D(pool_size=(2, 2))(x)
-    # x = Dropout(0.2)(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
     # x = Conv2D(filters=256,
     #            kernel_size=(3, 3),
     #            activation='relu')(x)
     # x = BatchNormalization()(x)
-    # x = MaxPooling2D(pool_size=(2, 2))(x)
+    # x = MaxPooling2D(pool_size=(0, 0))(x)
     x = Flatten()(x)
-    # x = Dropout(0.2)(x)
-    out = [Dense(36, name=f'digit{i+1}', activation='softmax')(x) for i in range(6)]
+    x = Dense(512, activation='relu')(x)
+    # x = Dropout(0.0)(x)
+    out = [Dense(len(alphabet), name=f'digit{i+1}', activation='softmax')(x) for i in range(6)]
     model = Model(main_input, out)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_digit1_acc', verbose=1, save_best_only=True, mode='max')
-    earlystop = EarlyStopping(monitor='val_digit1_acc', patience=5, verbose=1, mode='auto')
+    checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_digit1_accuracy', verbose=1, save_best_only=True, mode='max')
+    earlystop = EarlyStopping(monitor='val_digit1_accuracy', patience=10, verbose=1, mode='auto')
     tensorBoard = TensorBoard(log_dir=log_dir, histogram_freq=1)
     callbacks_list = [tensorBoard, earlystop, checkpoint]
+    # callbacks_list = [tensorBoard]
 
     model.summary()
     train_history = model.fit_generator(
@@ -108,6 +109,4 @@ def train():
 
 
 if __name__ == "__main__":
-    # a = generator('train/data01_train', 1)
-    # print(next(a)[0][0])
     train()
